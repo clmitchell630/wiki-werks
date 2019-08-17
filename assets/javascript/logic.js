f7cd3c2eba4e73ee9123d30d9813bed969e30b14
Mark Hartman <mthartman77@gmail.com>
2019-08-17 13:31:34 -0500

    Quasi-got the Wikipedia API functioning.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
     }
     //When something with id wiki-form is clicked, perform the following function.
     $("#find-wiki").on("click", function(event) {
-        event.preventdefault();
+        event.preventDefault();
         console.log('foo')
         //Defines a variable that takes the field text on the form and stores it.
         var WikiFormEntry = $(this).data("wiki-input");
@@ -50,22 +50,3 @@ $(document).ready(function () {
         })
     })
 })
-/*
-        $("#sidebar").mCustomScrollbar({
-             theme: "minimal"
-        });
-    
-        $('#sidebarCollapse').on('click', function () {
-            // open or close navbar
-            $('#sidebar').toggleClass('active');
-            // close dropdowns
-            $('.collapse.in').toggleClass('in');
-            // and also adjust aria-expanded attributes we use for the open/closed arrows
-            // in our CSS
-            $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-        });
-
-        
-    
-    });
-*/
