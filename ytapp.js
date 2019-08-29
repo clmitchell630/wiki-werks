9292d0edc14012410be5c5bebcf6d12e57b04a1c
cmitchell <clmitchell630@gmail.com>
2019-08-29 00:08:12 -0500

    found a bug with channels as results, fixed it

--- ytapp.js
+++ ytapp.js
@@ -17,18 +17,37 @@ $(function () {
                 var newItem = $("<li>").addClass("media");
                 var newDiv = $("<div>");
 
-
-                newItem.append(
-                    newDiv.addClass("card cardStyle").append(
-                        $("<a>").attr("href", "https://www.youtube.com/watch?v=" + response.items[i].id.videoId).attr("target", "_blank").append(
-                            $("<img>").attr("src", response.items[i].snippet.thumbnails.high.url).addClass("card-img-top").attr("alt", response.items[i].snippet.title),
-                            $("<div>").addClass("card-body").append(
-                            $("<h5>").addClass("card-text").html(response.items[i].snippet.title)
+                if (response.items[i].id.kind === "youtube#video") {
+                    newItem.append(
+                        newDiv.addClass("card cardStyle").append(
+                            $("<a>").attr("href", "https://www.youtube.com/watch?v=" + response.items[i].id.videoId).attr("target", "_blank").append(
+                                $("<img>").attr("src", response.items[i].snippet.thumbnails.high.url).addClass("card-img-top").attr("alt", response.items[i].snippet.title),
+                                $("<div>").addClass("card-body").append(
+                                    $("<h5>").addClass("card-text").html(response.items[i].snippet.title)
+                                )
                             )
-                        )
-                    ),
-                    
-                );
+
+                        ),
+
+                    );
+                }
+                else if (response.items[i].id.kind === "youtube#channel") {
+                    newItem.append(
+                        newDiv.addClass("card cardStyle").append(
+                            $("<a>").attr("href", "https://www.youtube.com/channel/" + response.items[i].id.channelId).attr("target", "_blank").append(
+                                $("<img>").attr("src", response.items[i].snippet.thumbnails.high.url).addClass("card-img-top").attr("alt", response.items[i].snippet.title),
+                                $("<div>").addClass("card-body").append(
+                                    $("<h5>").addClass("card-text").html(response.items[i].snippet.title)
+                                )
+                            )
+
+                        ),
+
+                    );
+                }
+                else{
+                    console.error("Hey something went wrong!");
+                }
 
                 $("#ytList").append(newItem);
                 $("#ytList").append($("<div>").attr("style", "height: 10px;"));
