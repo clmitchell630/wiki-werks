3aedd2e24ea6a234066b17093405956aa3b28c58
Magus <mfail792@gmail.com>
2019-08-31 15:18:01 -0500

    added validateForm() to start button, letsGo() inside of validateForm()

--- landing.js
+++ landing.js
@@ -9,49 +9,55 @@ anime({
         value: '2turn',
         easing: 'easeInOutSine'
     }
-    
+
 });
 
-function validateForm() {
-  var x = document.forms["myForm"]["fname"].value;
-  var y = document.forms["myForm"]["userEmail"].value;
- 
-  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//on click, run validateForm() function
+$('#start').click(function () {
+    validateForm();
+});
 
-  if (x == "" || y == "") {
-    $('#michell').text("All fields must be completed");
-    console.log ("test")
-    return false;
-}
-else if (y.match(mailformat)){ 
-    return true;
-
-    //anime here?
-
-} 
-else {
-    $('#michell').text("Please submit a properly formactted email address");
-    console.log ("test")
-    return false;
-
-} 
-
-}
-
-
-
-
-
-$('#start').click(function() {
+//creating function to take user to request.html
+function letsGo() {
     window.location = "./request.html";
- });
+}
+
+//checking UI validation/formatting
+function validateForm() {
+
+    var x = document.forms["myForm"]["fname"].value;
+    var y = document.forms["myForm"]["userEmail"].value;
+
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (x == "" || y == "") {
+        $('#michell').text("All fields must be completed");
+        console.log("test")
+        return false;
+    }
+
+    //added letsGo() function here to direct user to main page if UI validates correctly
+    else if (y.match(mailformat)) {
+        letsGo();
+        return true;
+
+    }
+    else {
+        $('#michell').text("Please submit a properly formatted email address");
+        console.log("test")
+        return false;
+
+    }
+
+}
 
 
 
 
 
- 
-   
+
+
+
 
 
 
