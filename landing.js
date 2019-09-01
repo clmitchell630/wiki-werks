b4745da4cdcc177b0c8d6778b29a3c47d8a48533
Magus <mfail792@gmail.com>
2019-09-01 00:23:39 -0500

    adjusted styling

--- landing.js
+++ landing.js
@@ -1,57 +1,101 @@
-anime({
-    targets: '.bttn',
-    loop: true,
-    translateY: [
-        { value: -100, duration: 800 },
-        { value: 0, duration: 4000 },
-    ],
-    rotate: {
-        value: '2turn',
-        easing: 'easeInOutSine'
-    }
 
-});
+//function for anime.js button shake upon failed UI validation
+function letsAnime2() {
+    const xMax = 16;
+    anime({
+        targets: '.bttn',
+        easing: 'easeInOutSine',
+        duration: 550,
+        translateX: [
+            {
+                value: xMax * -1,
+            },
+            {
+                value: xMax,
+            },
+            {
+                value: xMax / -2,
+            },
+            {
+                value: xMax / 2,
+            },
+            {
+                value: 0
+            }
+        ],
+    });
 
-//on click, run validateForm() function
-$('#start').click(function () {
-    validateForm();
-});
+}
 
-//creating function to take user to request.html
-function letsGo() {
+//function for button spinning animation for UI success (optional)
+function letsAnime() {
+
+    anime({
+
+        targets: '.bttn',
+        loop: false,
+        translateY: [
+            { value: -100, duration: 800 },
+            { value: 0, duration: 4000 },
+        ],
+        rotate: {
+            value: '2turn',
+            easing: 'easeInOutSine',
+
+        }
+
+    });
+
+}
+
+
+//function to direct to main page
+function winner() {
     window.location = "./request.html";
 }
 
-//checking UI validation/formatting
-function validateForm() {
+//on click, run validateForm() function
+$(document).ready(function () {
+    $('#start').click(function () {
+        validateForm();
+    });
 
-    var x = document.forms["myForm"]["fname"].value;
-    var y = document.forms["myForm"]["userEmail"].value;
+    //creating function to spin anime.js and take user to request.html
 
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-    if (x == "" || y == "") {
-        $('#michell').text("All fields must be completed");
-        console.log("test")
-        return false;
+    //checking UI validation/formatting
+    function validateForm() {
+
+        var x = document.forms["myForm"]["fname"].value;
+        var y = document.forms["myForm"]["userEmail"].value;
+
+        var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+        if (x == "" || y == "") {
+            $('#michell').text("All fields must be completed");
+            console.log("test");
+            letsAnime2();
+            return false;
+        }
+
+        //added letsGo() function here to direct user to main page if UI validates
+        else if (y.match(mailformat)) {
+            letsAnime();
+            return true;
+
+
+        }
+        else {
+            $('#michell').text("Please submit a properly formatted email address");
+            console.log("test")
+            letsAnime2();
+            return false;
+
+        }
+
     }
 
-    //added letsGo() function here to direct user to main page if UI validates correctly
-    else if (y.match(mailformat)) {
-        letsGo();
-        return true;
-
-    }
-    else {
-        $('#michell').text("Please submit a properly formatted email address");
-        console.log("test")
-        return false;
-
-    }
-
-}
-
-
+});
 
 
 
